5acda6ce1c0df4d1cfc629ff1662522f878f1cbb
nokklair <nokklair@proton.me>
2025-12-12 13:47:43 +0300

    Inventory package

---
+++ Scripts/Inventory.cs
@@ -0,0 +1,15 @@
+﻿/* 
+ *  Copyright © yiroth, all right reserved 
+ *  Creation date: 2025-11-25
+ *  Purpose: Inventory component
+*/
+
+using UnityEngine;
+
+namespace LibYiroth.Inventory
+{
+    public class Inventory : MonoBehaviour
+    {
+        
+    }
+}
